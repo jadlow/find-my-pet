97c1ecb1d80542150692dae181a9976bc86e2651
Chen <jwang344@ucsc.edu>
2022-05-05 15:34:28 -0700

    set search dist range min 0.5 max 20 def 10; added show range miles

---
+++ static/js/mainjs.js
@@ -0,0 +1,4 @@
+// function to change the displaying of the distance range slider value
+function changeDistRangeDisplay(miles){
+    document.querySelector("#distRangeDisplay").innerHTML = miles;
+}
