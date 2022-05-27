d786fe6f32747beb4bbc963c6226fd174c5b596c
Chen <jwang344@ucsc.edu>
2022-05-27 09:06:57 -0700

    add.html & add_pet.js (fixed check is not a number logic for coordinates and date; added coords preview)

--- static/js/add_pet.js
+++ static/js/add_pet.js
@@ -19,6 +19,7 @@ let init = (app) => {
         new_pet_lost: "true",
         new_pet_description: "",
         new_pet_lat_lng: "",
+        err_main: false,
         err_name: false,
         err_name_i: "",
         err_type: false,
@@ -32,6 +33,7 @@ let init = (app) => {
         err_description_i: "",
         err_coord: false,
         err_coord_i: "",
+        location_preview_zoom: 12,
     };
 
     app.enumerate = (a) => {
@@ -43,6 +45,7 @@ let init = (app) => {
 
 
     app.new_post = function(){
+        app.vue.err_main = false;
         app.vue.err_name = false;
         app.vue.err_type = false;
         app.vue.err_lost = false;
@@ -54,6 +57,7 @@ let init = (app) => {
         if(app.vue.new_pet_name.replace(/\s/g, "").length == 0){
             app.vue.err_name_i = "empty pet name. Please specify a pet name that contains 1 or more characters."
             app.vue.err_name = true;
+            app.vue.err_main = true;
             return -1;
         }
 
@@ -61,6 +65,7 @@ let init = (app) => {
         if(!["Dog", "Cat", "Bird", "Rodent", "Reptile", "Rabbit", "Pet Livestock", "Aquatic", "Other"].includes(app.vue.new_pet_type)){
             app.vue.err_type_i = "invalid pet type. Please select a pet type from the given options."
             app.vue.err_type = true;
+            app.vue.err_main = true;
             return -1;
         }
 
@@ -68,6 +73,7 @@ let init = (app) => {
         if(!["true", "false"].includes(app.vue.new_pet_lost)){
             app.vue.err_lost_i = "invalid pet lost or found value. Please select whether the pet was lost or found.";
             app.vue.err_lost = true;
+            app.vue.err_main = true;
             return -1;
         }
         let new_pet_lost_bool = true;
@@ -76,32 +82,37 @@ let init = (app) => {
         }
 
         // Check date.
-        if(isNaN(app.vue.new_pet_lostfound_date_m) || (app.vue.new_pet_lostfound_date_m.length == 0)){
+        if(isNaN(app.vue.new_pet_lostfound_date_m) || isNaN(parseInt(app.vue.new_pet_lostfound_date_m))){
             app.vue.err_date_i = "month is not a number. Please specify a month with a number.";
             app.vue.err_date = true;
+            app.vue.err_main = true;
             return -1;
         }
         app.vue.new_pet_lostfound_date_m = parseInt(app.vue.new_pet_lostfound_date_m);
         if((app.vue.new_pet_lostfound_date_m < 1) || (app.vue.new_pet_lostfound_date_m > 12)){
             app.vue.err_date_i = "month not in range. Please specify a month with a number between 1 to 12.";
             app.vue.err_date = true;
+            app.vue.err_main = true;
             return -1;
         }
-        if(isNaN(app.vue.new_pet_lostfound_date_d) || (app.vue.new_pet_lostfound_date_d.length == 0)){
+        if(isNaN(app.vue.new_pet_lostfound_date_d) || isNaN(parseInt(app.vue.new_pet_lostfound_date_d))){
             app.vue.err_date_i = "date is not a number. Please specify a date with a number.";
             app.vue.err_date = true;
+            app.vue.err_main = true;
             return -1;
         }
         app.vue.new_pet_lostfound_date_d = parseInt(app.vue.new_pet_lostfound_date_d);
-        if(isNaN(app.vue.new_pet_lostfound_date_y) || (app.vue.new_pet_lostfound_date_y.length == 0)){
+        if(isNaN(app.vue.new_pet_lostfound_date_y) || isNaN(parseInt(app.vue.new_pet_lostfound_date_y))){
             app.vue.err_date_i = "year is not a number. Please specify a year with a number";
             app.vue.err_date = true;
+            app.vue.err_main = true;
             return -1;
         }
         app.vue.new_pet_lostfound_date_y = parseInt(app.vue.new_pet_lostfound_date_y);
         if((app.vue.new_pet_lostfound_date_y < 2012) || (app.vue.new_pet_lostfound_date_y > 2022)){
             app.vue.err_date_i = "year not in range. Please specify a year with a number between 2012 to 2022.";
             app.vue.err_date = true;
+            app.vue.err_main = true;
             return -1;
         }
         let month_upp = -1;
@@ -120,11 +131,13 @@ let init = (app) => {
         if(in_date.getTime() > today_date.getTime()){
             app.vue.err_date_i = "date is in the future. Please specify a date between January 1, 2012 to today.";
             app.vue.err_date = true;
+            app.vue.err_main = true;
             return -1;
         }
         if(app.vue.new_pet_lostfound_date_d < 1 || app.vue.new_pet_lostfound_date_d > month_upp){
             app.vue.err_date_i = "date not in range. Please specify a date with a number between 1 and " + month_upp + ".";
             app.vue.err_date = true;
+            app.vue.err_main = true;
             return -1;
         }
 
@@ -132,6 +145,7 @@ let init = (app) => {
         if(app.vue.new_pet_description.replace(/\s/g, "").length == 0){
             app.vue.err_description_i = "description is empty. Please provide a description about your pet.";
             app.vue.err_description = true;
+            app.vue.err_main = true;
             return -1;
         }
 
@@ -141,21 +155,24 @@ let init = (app) => {
         if(new_pet_lat_lng_split.length != 2){
             app.vue.err_coord_i = "coordinate length invalid. Please specify a coordinate with two numbers separated by a comma.";
             app.vue.err_coord = true;
+            app.vue.err_main = true;
             return -1;
         }
         let new_pet_lat = new_pet_lat_lng_split[0];
         let new_pet_lng = new_pet_lat_lng_split[1];
-        if(isNaN(new_pet_lat)){
-            app.vue.err_coord_i = "latutide is not a number. Please specify a number for the latitude.";
+        if(isNaN(new_pet_lat) || isNaN(parseFloat(new_pet_lat))){
+            app.vue.err_coord_i = "latitude is not a number. Please specify a number for the latitude.";
             app.vue.err_coord = true;
+            app.vue.err_main = true;
             return -1;
         }
         else{
             new_pet_lat = parseFloat(new_pet_lat);
         }
-        if(isNaN(new_pet_lng)){
+        if(isNaN(new_pet_lng) || isNaN(parseFloat(new_pet_lng))){
             app.vue.err_coord_i = "longitude is not a number. Please specify a number for the longitude.";
             app.vue.err_coord = true;
+            app.vue.err_main = true;
             return -1;
         }
         else{
@@ -164,11 +181,13 @@ let init = (app) => {
         if(new_pet_lat < -90 || new_pet_lat > 90){
             app.vue.err_coord_i = "latitude is not in range. Please specify a number for the latitude between -90 to 90.";
             app.vue.err_coord = true;
+            app.vue.err_main = true;
             return -1;
         }
         if(new_pet_lng < -180 || new_pet_lng > 180){
             app.vue.err_coord_i = "longitude is not in range. Please specify a number for the longitude between -180 to 180.";
             app.vue.err_coord = true;
+            app.vue.err_main = true;
             return -1;
         }
 
@@ -189,10 +208,32 @@ let init = (app) => {
         app.vue.post_success = true;
     }
 
+    app.location_preview = function(){
+        document.getElementById("display_location").style.border="3px solid #60cc60";
+        const new_pet_lat_lng_cleaned = app.vue.new_pet_lat_lng.replace(/\s/g, "");
+        const new_pet_lat_lng_split = new_pet_lat_lng_cleaned.split(",");
+        if(new_pet_lat_lng_split.length != 2){
+            document.getElementById("display_location").style.border="3px solid #d00000";
+            return -1;
+        }
+        const new_pet_lat = new_pet_lat_lng_split[0];
+        if(isNaN(new_pet_lat) || isNaN(parseFloat(new_pet_lat)) || parseFloat(new_pet_lat) < -90 || parseFloat(new_pet_lat) > 90){
+            document.getElementById("display_location").style.border="3px solid #d00000";
+            return -1;
+        }
+        const new_pet_lng = new_pet_lat_lng_split[1];
+        if(isNaN(new_pet_lng) || isNaN(parseFloat(new_pet_lng)) || parseFloat(new_pet_lng) < -180 || parseFloat(new_pet_lng) > 180){
+            document.getElementById("display_location").style.border="3px solid #d00000";
+            return -1;
+        }
+        document.getElementById("display_location").src = "https://maps.google.com/maps?q=" + new_pet_lat + "," + new_pet_lng + "&z=" + app.vue.location_preview_zoom + "&output=embed";
+    }
+
     // This contains all the methods.
     app.methods = {
         // Complete as you see fit.
         add_post: app.new_post,
+        location_preview: app.location_preview,
     };
 
     // This creates the Vue instance.
